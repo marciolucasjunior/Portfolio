233247be77d92ad31b8cc7090f11c3088a926103
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-03-08 08:48:02 -0300

    Reorganizando o codigo

---
+++ main.js
@@ -0,0 +1,19 @@
+function Mudoutamanho(){
+    if (window.innerWidth >= 800) {
+        drop.style.display = 'none'
+    }
+ }   
+
+ function Menu(){
+    
+            
+         if (drop.style.display !='block'){
+            drop.style.display = 'block'
+         } else {
+            drop.style.display = 'none'
+         }    
+       
+      
+   
+   
+   }
